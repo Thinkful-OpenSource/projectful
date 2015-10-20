796352f3e99b7eb2c47be845d2e3e1dbfd503077
Aric Johnson <aric87@comcast.net>
2015-10-19 20:39:33 -0400

    Project Restart. Going to start over without CLI, Angular, and try to get more student involvement by simplifying the workflow.

--- client/app/course/course.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('projectfulApp')
-  .factory('Course', function Course($resource) {
-    return $resource('/api/courses/:id/:controller', {
-      id: '@_id'
-    });
-  });
